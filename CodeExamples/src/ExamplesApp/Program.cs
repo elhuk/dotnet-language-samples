f7bad58853d88ef39caa8d1d6efbdc22e0335f2d
Lindokuhle Mncwabe <Lindokuhle.Mncwabe@investec.co.za>
2023-08-09 22:22:18 +0200

    db added

--- CodeExamples/src/ExamplesApp/Program.cs
+++ CodeExamples/src/ExamplesApp/Program.cs
@@ -7,8 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<OptionalTypeEfDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("Default")));
+//builder.Services.AddDbContext<OptionalTypeEfDbContext>(options =>
+//    options.UseSqlite(builder.Configuration.GetConnectionString("Default")));
 
 
 var app = builder.Build();
